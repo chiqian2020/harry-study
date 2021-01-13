581452e1745b73794ba4dcd1cc794f0a060d95a5
冯小波 <fengxiaobo@shizhuang-inc.com>
2021-01-13 11:03:41 +0800

    test框架

---
+++ src/main/java/com/harry/study/AlgorithmStudyTest.java
@@ -0,0 +1,18 @@
+package com.harry.study;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * AlgorithmStudyTest
+ *
+ * @author harry
+ * @date 2021/1/13 10:54 上午
+ **/
+public class AlgorithmStudyTest {
+
+    @Test
+    public void test1() {
+        System.out.println("agdjasdjgasjd".hashCode());
+    }
+
+}

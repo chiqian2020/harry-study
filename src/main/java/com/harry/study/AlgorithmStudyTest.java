ad019c2e4ac572aff7d7370d8699805057d6f91e
冯小波 <fengxiaobo@shizhuang-inc.com>
2021-01-13 14:03:32 +0800

    test框架

--- src/main/java/com/harry/study/AlgorithmStudyTest.java
+++ src/main/java/com/harry/study/AlgorithmStudyTest.java
@@ -1,9 +1,11 @@
 package com.harry.study;
 
+import org.apache.commons.codec.digest.Md5Crypt;
 import org.junit.jupiter.api.Test;
 
 /**
- * AlgorithmStudyTest
+ *
+ * 死磕算法
  *
  * @author harry
  * @date 2021/1/13 10:54 上午
@@ -12,7 +14,8 @@ public class AlgorithmStudyTest {
 
     @Test
     public void test1() {
-        System.out.println("agdjasdjgasjd".hashCode());
+        String md5 = Md5Crypt.md5Crypt("agdjasdjgasjd".getBytes());
+        System.out.println(md5.hashCode());
     }
 
 }

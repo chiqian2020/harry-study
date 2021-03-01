abb2f21f0e31801ed7bcd4295c7578a705a6c9bc
冯小波 <fengxiaobo@shizhuang-inc.com>
2021-03-01 17:58:49 +0800

    算法学习

--- src/main/java/com/harry/study/AlgorithmStudyTest.java
+++ src/main/java/com/harry/study/AlgorithmStudyTest.java
@@ -1,8 +1,6 @@
 package com.harry.study;
 
-import org.apache.commons.codec.digest.Md5Crypt;
 import org.junit.jupiter.api.Test;
-
 import java.util.*;
 
 /**
@@ -42,7 +40,9 @@ public class AlgorithmStudyTest {
         System.out.println(str);
     }
 
-    //求最大长度不重复字串
+    /**
+     * 求最大不重复的字串
+     */
     @Test
     public void getMaxNotRepeatLength() {
         String str = "jsagdfjsgfkjsafuwifsjhbcsjbc";
@@ -60,7 +60,33 @@ public class AlgorithmStudyTest {
         System.out.println(max);
     }
 
-    //两个排序数组合并
+    /**
+     * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，
+     * 但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字
+     *
+     *
+     */
+    @Test
+    public void findRepeatNumber() {
+        int[] nums = new int[]{0,2,5,2,3,4,4};
+        int temp;
+        for (int i = 0;i < nums.length;i++) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    System.out.println(nums[i]);
+                    break;
+                }
+                temp = nums[i];
+                nums[i] = nums[temp];
+                nums[temp] = temp;
+            }
+        }
+    }
+
+    /**
+     * 排序组合合并
+     *
+     */
     @Test
     public void twoSortArrayMerge() {
         Integer[] array1 = {1,2,3,4};
@@ -92,6 +118,57 @@ public class AlgorithmStudyTest {
             }
         }
         System.out.println(Arrays.toString(array));
+    }
+
+    /**
+     * 数组中有个数字超过半数重复，请找出来
+     */
+    @Test
+    public void testFindRepeatNum() {
+        int[] nums = new int[]{4,4,6,1,4};
+        int result = nums[0];
+        int count = 1;
+        for (int i = 1;i<nums.length;i++) {
+            if (nums[i] == result) {
+                count++;
+            } else {
+                count--;
+            }
+            if (count == 0) {
+                result = nums[i];
+                count = 1;
+            }
+        }
+        System.out.println(result);
+    }
+
+    /**
+     * 数组中数字成对出现，只有两个是不成对的，找出来
+     */
+    @Test
+    public void test1SizeNum() {
+        int sum = 0;
+        int[] nums = new int[]{4,4,6,1};
+        for (int i = 0;i<nums.length;i++) {
+            sum ^= nums[i];
+        }
+        System.out.println(sum);
+
+        int first = 1;
+        while ((sum&first) == 0) {
+            first = first << 1;
+        }
+        System.out.println(first);
+        int[] result=new int[2];
+        for(int i=0;i<nums.length;i++){
+            //将数组分类。
+            if((nums[i]&first)==0){
+                result[0]^=nums[i];
+            } else{
+                result[1]^=nums[i];
+            }
+        }
+        System.out.println(result[0] +"----" + result[1]);
     }
 
 }

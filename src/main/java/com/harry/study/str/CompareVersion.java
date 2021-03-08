d690571b5ffc8c316017ce3f53cd5933edd05c11
冯小波 <fengxiaobo@shizhuang-inc.com>
2021-03-08 14:00:34 +0800

    版本号比较

--- src/main/java/com/harry/study/str/CompareVersion.java
+++ src/main/java/com/harry/study/str/CompareVersion.java
@@ -11,18 +11,21 @@ public class CompareVersion {
 
     public static void main(String[] args) {
         String v1 = "2.3.5";
-        String v2 = "3.0";
+        String v2 = "2.3.5";
         System.out.println(compareV(v1,v2));
     }
 
     private static int compareV(String v1, String v2) {
-        int v1Length = v1.length();
-        int v2Length = v2.length();
-        int i = 0;
-        int j = 0;
-        while (i < v1Length || j<v2Length) {
-            int a = 0;
-            int b = 0;
+        String[] version1 = v1.split("\\.");
+        String[] version2 = v2.split("\\.");
+        for (int i = 0,j = 0;i < version1.length || j< version2.length;i++,j++) {
+            int n1 = i < version1.length?Integer.valueOf(version1[i]):0;
+            int n2 = j < version2.length?Integer.valueOf(version2[i]):0;
+            if (n1 == n2) {
+                continue;
+            } else {
+                return n1 > n2?1:-1;
+            }
         }
         return 0;
     }
